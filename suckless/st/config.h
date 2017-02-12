ffb58e254e1cdcc9886783c3c19ae61f4837b439
druuu <dinesh@micropyramid.com>
2017-02-12 12:12:40 +0530

    st fonts, awesome

--- suckless/st/config.h
+++ suckless/st/config.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "druuu Mono:pixelsize=14:antialias=true:autohint=true";
+//static char font[] = "druuu Mono:pixelsize=14:antialias=true:autohint=true";
+static char font[] = "-*-druuu-*-*-semicondensed-*-14-*-*-*-*-*-*-*";
 static int borderpx = 0;
 static int histsize = 2000;
 
