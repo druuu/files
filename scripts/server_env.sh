36f3208ae781ac24dec47aa7082c73cf0e4ad40a
druuu <dinesh@micropyramid.com>
2017-03-08 23:21:55 +0530

    server_env changes

--- scripts/server_env.sh
+++ scripts/server_env.sh
@@ -4,9 +4,8 @@ run(){
     wdir=`pwd` &&
     tmp_dir=`mktemp -d` &&
     cd "$tmp_dir" &&
-    curl -o server_vimrc 'https://raw.githubusercontent.com/druuu/files/master/vi/server_vimrc' &&
-    
-    alias vim="vim -u {$tmp_dir}/server_vimrc" &&
+    curl -s -o server_vimrc 'https://raw.githubusercontent.com/druuu/files/master/vi/server_vimrc' &&
+    alias vi="vim -u ${tmp_dir}/server_vimrc" &&
     PS1='\[\e[0;15m\]\h \[\e[0;34m\]${VIRTUAL_ENV##*/} \[\e[0;33m\]\W\[\e[0m\] \[\e[0;15m\]' &&
     set -o vi &&
     export TERM=xterm-256color &&
