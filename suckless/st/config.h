a9d770d01010c0910857e995c64434abc98f4e7e
druuu <dinesh@micropyramid.com>
2017-03-26 15:41:35 +0530

    st 1.1 hscale

--- suckless/st/config.h
+++ suckless/st/config.h
@@ -26,8 +26,8 @@ static char stty_args[] = "stty raw pass8 nl -echo -iexten -cstopb 38400";
 static char vtiden[] = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
-static float cwscale = 0.86;
-static float chscale = 1.0;
+static float cwscale = 1.0;
+static float chscale = 1.1;
 
 /*
  * word delimiter string
