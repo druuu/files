e82701c1f8968338ff040780bb1dfd3a8653a6f8
druuu <dinesh@micropyramid.com>
2017-09-09 20:00:13 +0530

    st notification bar

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -19,6 +19,7 @@ static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
+	{ "stn",     NULL,       NULL,       0,            True,        -1 },
 };
 
 /* layout(s) */
