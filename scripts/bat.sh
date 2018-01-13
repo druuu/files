492449645dffb4584d79f8e2604c1cbba7190f4e
d <d@d.com>
2018-01-13 12:41:49 +0530

    modified bat.sh

--- scripts/bat.sh
+++ scripts/bat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-capacity=`cat /sys/class/power_supply/BAT1/capacity`
-status=`cat /sys/class/power_supply/BAT1/status`
+capacity=`cat /sys/class/power_supply/BAT0/capacity`
+status=`cat /sys/class/power_supply/BAT0/status`
 notification="Capacity: ${capacity}%"
 #acpi_time=`acpi | sed -n 's/\(.*%, \(.*\) until .*\)\|\(.*%, \(.*\) .*\)/\2\4/p'`
 
@@ -13,9 +13,9 @@ then
     fi
     if [ $capacity -le 35 ]
     then
-        stn -i -g 25x1+1187 -e /root/.config/stn_bat.sh " bat discharging: $capacity%" &
+        /usr/local/bin/stn -i -g 25x1+1187 -e /root/.config/stn_bat.sh " bat discharging: $capacity%" &
     fi
 elif [ $capacity -ge 85 ]
 then
-    stn -i -g 25x1+1187 -e /root/.config/stn_bat.sh " bat charging: $capacity%" &
+    /usr/local/bin/stn -i -g 25x1+1187 -e /root/.config/stn_bat.sh " bat charging: $capacity%" &
 fi
